420e84f2b5baa47e6c29ead5b17445737f2b7d15
Zhou Jin An <c24712003@yahoo.com.tw>
2022-06-16 21:44:32 +0800

    Angular-s2 button"clear"清除頁面底部裡的前五筆訊息

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ConfirmTemplateDialog } from './dialog/confirm-template/confirm-template.dialog';
+import { MessageService } from './message.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
+export class AppComponent implements OnInit {
+  title = 'Careline - Tour of Heroes';
+
+  constructor(public messageService: MessageService, public dialog: MatDialog) { }
+  ngOnInit(): void {
+  }
+
+  clearTopFourMsg() {
+    const dialogRef = this.dialog.open(ConfirmTemplateDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let length = this.messageService.messages.length;
+
+        if (length <= 5) {
+          this.messageService.clear();
+        } else {
+          this.messageService.messages.splice(0, 5);
+          this.messageService.messages;
+        }
+      }
+    });
+  }
 }
